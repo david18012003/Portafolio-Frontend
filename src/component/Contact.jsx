57dc7b891bae0b1919ba414a02ee3ca5a03af11f
david18012003 <bravocarmen264@gmail.com>
2025-06-15 17:53:24 -0500

    Actualizaci├│n autom├ítica:  - 15/06/2025 17:53:24,63 frontend

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -4,8 +4,8 @@ export default function Contact({phone, email, linkedin, github}) {
         <h2>Contacto</h2>
         <p>Email: {email}</p>
         <p>Teléfono: {phone}</p>
-        <a href={linkedin} target="_blank">LinkedIn</a>
-        <a href={github} target="_blank">GitHub</a>
+        <p><a href={linkedin} target="_blank">LinkedIn</a></p>
+        <p><a href={github} target="_blank">GitHub</a></p>
       </section>
     )
   }
